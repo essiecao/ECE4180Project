a58f7b0de6830f4b8a50fa5dea6d109dfc884936
KevinSu520 <94208936+KevinSu520@users.noreply.github.com>
2024-04-22 16:25:20 -0400

    Update ECE4180FinalProject.cpp
    complete working code on the mbed

--- ECE4180FinalProject.cpp
+++ ECE4180FinalProject.cpp
@@ -1,3 +1,4 @@
+
 #include "Thread.h"
 #include "mbed.h"
 #include "rtos.h"
@@ -6,7 +7,12 @@
 #include "ultrasonic.h"
 #include "Motor.h"
 #include "SongPlayer.h"
+#include <algorithm>
 
+
+//Pi mbed USB Slave function
+// connect mbed to Pi USB
+RawSerial  pi(USBTX, USBRX);
 
 PwmOut speaker(p26);
 Motor m(p21, p6, p5); // pwm, fwd, rev , left motor
@@ -15,6 +21,11 @@ Serial bluetooth(p28, p27); //bluetooth controller
 
 Mutex motor_mutex;
 volatile int sonar_distance;
+volatile int pi_instruction;
+
+//for motor inputs;
+volatile float motor_1_movement;
+volatile float motor_2_movement;
 
 
 
@@ -30,19 +41,83 @@ ultrasonic mu(p11, p12, .1, 1, &dist);
 void motor_control(void const *args) {
 
     while(1) {
-    // for (float s= -1.0; s < 1.0 ; s += 0.01) {
-    //    m.speed(s); 
-    //    m2.speed(s);
-    //    Thread::wait(20);
-    // }
+    motor_mutex.lock();
+    m.speed(motor_1_movement);
+    m2.speed(motor_1_movement);
+    Thread::wait(20);
+    motor_mutex.unlock();
     }
 
 }
 void serial_control(void const *agrs) {
+    char bnum=0;
+    char bhit=0;
+    while(1) {
+        while (!pi.readable()) {
+        Thread::wait(20);
+        }
+      if (pi.getc()=='!') {
+            if (pi.getc()=='B') { //button data packet
+                bnum = pi.getc(); //button number
+                bhit = pi.getc(); //1=hit, 0=release
+               
+                    switch (bnum) {
+
+                        case '1': //button 1 will stop the robot
+                        if(bhit == '1') {
+                            motor_1_movement = 0;
+                            motor_2_movement = 0;
+                        }
+
+                        break;
+                        
+                        case '5': //forward, up arrow
+                        if (bhit=='1'){      
+                        motor_1_movement = -0.5;
+                        motor_2_movement = -0.5;
+                        }
+                            
+                            break;
+                       
+                        case '6': //backward down arrow
+                         if (bhit=='1'){
+                        motor_1_movement = 0.5;
+                        motor_2_movement = 0.5;
+                        }
+                        
+                          
+                            break;
+                        case '7': //left
+                         if (bhit=='1'){
+                        motor_1_movement = -0.3;
+                        motor_2_movement = 0.3;
+
+                        }
+
+                            break;
+
+                        case '8': //right
+                         if (bhit=='1'){
+                        motor_1_movement = 0.3;
+                        motor_2_movement = -0.3;
+
+                        }
+
+                            break;
+                        
+                        default:
+                            break;
+                    
+                }
+            }
+        }
+        motor_mutex.unlock();
+    }
+
 
 }
 void bluetooth_control(void const *args) {
-     char bnum=0;
+    char bnum=0;
     char bhit=0;
     while(1) {
         while (!bluetooth.readable()) {
@@ -67,8 +142,8 @@ void bluetooth_control(void const *args) {
                         
                         case '5': //forward, up arrow
                         if (bhit=='1'){      
-                        m.speed(-0.2);
-                        m2.speed(-0.2);
+                        m.speed(-0.5);
+                        m2.speed(-0.5);
                         }
                             
                             break;
@@ -83,8 +158,8 @@ void bluetooth_control(void const *args) {
                             break;
                         case '7': //left
                          if (bhit=='1'){
-                             m.speed(0);
-                             m2.speed(0.2);
+                             m.speed(-0.3);
+                             m2.speed(0.3);
 
                         }
 
@@ -92,8 +167,8 @@ void bluetooth_control(void const *args) {
 
                         case '8': //right
                          if (bhit=='1'){
-                             m.speed(0.2);
-                             m2.speed(0);
+                             m.speed(0.3);
+                             m2.speed(-0.3);
 
                         }
 
@@ -119,16 +194,33 @@ void sonar_control(void const *args) {
 
 
 void speaker_control(void const *args) {
-    while(1) {
-    // speaker.period(1.0/500.0); // 500hz period
-    //    speaker = 0.5;
-    //    Thread::wait(2000);
-    //    speaker = 0.0;
-    //    Thread::wait(2000);
+     while(1) {
+        int local_sonar_distance;
+
+        // Lock the mutex to safely read sonar_distance
+        motor_mutex.lock();
+        local_sonar_distance = sonar_distance;
+        motor_mutex.unlock();
+
+        // Calculate beep period and duration based on the distance
+        float beep_period;
+        if (local_sonar_distance > 1000) {
+            beep_period = 1.0; // slower beep, 1 second interval
+        } else {
+            // As distance decreases, increase the frequency of the beeps
+            // For example, at 100 mm distance, the period could be very short.
+            beep_period = max(0.1,(local_sonar_distance / 1000.0));
+        }
+
+        // Set the speaker to beep
+        speaker.period(1.0 / 500.0); // Set frequency of the beep
+        speaker = 0.5; // Turn on speaker at 50% duty cycle
+        Thread::wait(beep_period *200); // Wait for the same period to complete the cycle
+        speaker = 0.0; // Turn off speaker
+        Thread::wait(beep_period *200);
+       
     }
 }
-
-
 
 
 int main()
@@ -137,7 +229,6 @@ int main()
     mu.startUpdates();//start measuring the distance
   
 
-
     Thread thread2(motor_control);
     Thread thread3(serial_control);
     Thread thread4(bluetooth_control);
@@ -145,7 +236,7 @@ int main()
     Thread thread6(speaker_control);
 
     while(1) {
-        //do stuff
+        //nihao 
         
     }
  
